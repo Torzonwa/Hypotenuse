26f099cab62a7ca865b4c3e4baef7f449cd99c86
William Torzon <torzonwa@grace.edu>
2020-09-09 12:30:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA;
+            double sideB; 
+            double hypo;
+            Console.Write("Side A : ");
+                sideA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Side B : ");
+                sideB = Convert.ToDouble(Console.ReadLine());
+                hypo = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+                Console.WriteLine("Hypotenuse = " + hypo);
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+
